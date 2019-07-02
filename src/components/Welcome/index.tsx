dfaedf78a19d6bd13fa0ec99b4678b92a7f5a24b
Julio Henriquez <julio.m.henriquez@gmail.com>
2019-07-01 20:49:57 -0400

    Adds typewriter animation.

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,10 +1,35 @@
 import './Welcome.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Welcome: React.FC = () => {
+  const commands = [
+    '$ yarn create react-app',
+    '$ git init',
+    '$ git commit',
+    '$ git push',
+  ];
+
+  const [currentCommand, setCurrentCommand] = useState(0);
+
+  setTimeout(() => {
+    const newCommand = currentCommand + 1;
+    if (newCommand >= commands.length) {
+      return setCurrentCommand(0);
+    }
+    setCurrentCommand(newCommand);
+  }, 15000);
+
   return (
     <section className="welcome">
-      <h1>Engineer.</h1>
+      <h1>Engineer</h1>
+      <div className="typewriter">
+        <span>{commands[currentCommand]}</span>
+        <span className="console-cursor">_</span>
+      </div>
+      {/* <div className="social-icons">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg" />
+        <img src="https://unpkg.com/simple-icons@latest/icons/linkedin.svg" />
+      </div> */}
     </section>
   );
 }
