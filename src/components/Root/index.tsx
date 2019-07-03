54c60b5b48aa29b4883e570a0ee95b37663c1c0f
Julio Henriquez <julio.m.henriquez@gmail.com>
2019-07-02 23:42:51 -0400

    Adds react-transition-group to handle route smooth route changes.

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Menu from '../Menu';
 import Welcome from '../../views/Welcome';
+import About from '../../views/About';
 import Footer from '../Footer';
 
 const Root: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/:activeItem?" component={Menu} />
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/welcome" component={Welcome} />
-      </Switch>
+      <Route render={({location}) => (
+        <TransitionGroup enter={true} exit={false} appear={true}>
+          <CSSTransition
+            key={location.key}
+            timeout={500}
+            classNames="view"
+            >
+            <Switch location={location}>
+              <Route exact path="/" component={Welcome} />
+              <Route exact path="/welcome" component={Welcome} />
+              <Route exact path="/about" component={About} />
+            </Switch>  
+          </CSSTransition>
+        </TransitionGroup>
+      )}/>
       <Footer />
     </BrowserRouter>
   );
